4516ee579f5dd90ef2fce1998d98486bafa607cd
Rubi-areli <rubita.alvarenga@gmail.com>
2024-10-16 12:53:21 -0600

    ejemplo2.py

--- ejemplo2.py
+++ ejemplo2.py
@@ -1,3 +1,9 @@
+#El objetivo es abrir una imagen, convertirla
+#a escala de grises y guardarla con un nuevo nombre, 
+#manteniendo la imagen original intacta. 
+#Además, muestra tanto la imagen original como la versión en escala de grises.
+#
+
 from PIL import Image
 
 # Abre la imagen desde la ruta completa
